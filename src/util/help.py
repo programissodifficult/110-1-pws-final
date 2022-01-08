2a097364b08692e01b2f30e1c8c4daa9d8bd2169
programissodifficult <b10701009anhelin@gmail.com>
2022-01-08 13:35:15 +0800

    game guide added

--- src/util/help.py
+++ src/util/help.py
@@ -5,4 +5,4 @@ from .Dialog import confirm, yesno
 def help():
     result = yesno('開啟網頁版說明書', '即將在瀏覽器開啟網頁版說明書，是否繼續?')
     if result:
-        webbrowser.open('https://miccwan.github.io/110-1-pws-final/')
+        webbrowser.open('https://i.imgur.com/od97sfg.jpg')
